c7d1c638110f9aae710b09f6636097a0608b9361
Vitaly Kuzyaev <vitkuz573@gmail.com>
2026-06-11 15:01:58 +0500

    Remove test files

--- test_config.py
+++
@@ -1,8 +0,0 @@
-from application.config import ConfigService
-import json
-
-cs = ConfigService()
-opts = cs.get_options()
-with open('config_options.json', 'w', encoding='utf-8') as f:
-    json.dump(opts['mailbox_providers'], f, ensure_ascii=False, indent=2)
-print('Saved to config_options.json')
